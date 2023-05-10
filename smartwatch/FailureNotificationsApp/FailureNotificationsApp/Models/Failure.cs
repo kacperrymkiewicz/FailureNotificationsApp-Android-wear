6db4fbf58ba14e27e5599e3f6d21088ecef281df
Kacper <kacperrymkiewicz@gmail.com>
2023-05-10 11:33:22 +0200

    Add failure model

---
+++ smartwatch/FailureNotificationsApp/FailureNotificationsApp/Models/Failure.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FailureNotificationsApp.models
+{
+    public class Failure
+    {
+        public int id { get; set; }
+        public int mozna_pracowac { get; set; }
+        public string opis_awarii { get; set; }
+        public int status { get; set; }
+        public int priorytet { get; set; }
+        public DateTime data_zgloszenia { get; set; }
+        public object data_podjecia { get; set; }
+        public object data_naprawy { get; set; }
+        public Workstation stanowisko { get; set; }
+    }
+}
---
+++ smartwatch/FailureNotificationsApp/FailureNotificationsApp/Models/Workstation.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FailureNotificationsApp.models
+{
+    public class Workstation
+    {
+        public int id { get; set; }
+        public string kod { get; set; }
+        public string nazwa { get; set; }
+        public string opis { get; set; }
+    }
+}
